9ad79ae4c272ce24b80df14b2e2d4b09664ff501
limenghui <1479856243@qq.com>
2018-12-21 19:58:25 +0800

    1958

---
+++ src/components/cubeComponents/cube.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="cube"></div>
+</template>
+<style lang="scss" scoped>
+    @import './cube.scss';
+</style>
+<script>
+//滴滴团队基于vue应用的ui框架https://didi.github.io/cube-ui/#/zh-CN/docs/introduction
+import Vue from 'vue';
+export default {
+    data(){
+        return{
+
+        }
+    },
+    beforeMount(){},
+    mounted(){},
+    methods(){}
+}
+</script>
+
+
