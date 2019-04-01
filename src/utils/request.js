25e0f98fee862d612fc8b8de555348e5e43b90ac
limenghui <1479856243@qq.com>
2019-04-01 10:39:46 +0800

    解决axios默认请求options及合并了vue-ssr代码的问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
  
 Vue.use(VueAxios, axios);
-
+//解决axiox默认请求options的方法
+axios.defaults.headers = {
+	"Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+};
 //定义请求服务器公共域及服务名称
 // const publicUrl='http://23w4962t28.imwork.net:44586';
 const publicUrl =`http://www.jasonlee.top/`;
